e03c9cd4a10f8c199a8eab1b5dc28e7272a5fed5
king <rohanbindal1149@gmail.com>
2020-10-29 11:06:32 +0530

    f

--- app/src/main/java/com/infinitysensei/kaguyasamawallpaper/splash.java
+++ app/src/main/java/com/infinitysensei/kaguyasamawallpaper/splash.java
@@ -1,4 +1,41 @@
 package com.infinitysensei.kaguyasamawallpaper;
 
-public class splash {
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 15000;
+    ImageView imageView;
+    /** Called when the activity is first created.**/
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        imageView=(ImageView)findViewById(R.id.imageView1);
+        Animation an2= AnimationUtils.loadAnimation(this,R.anim.life_to_right);
+        imageView.startAnimation(an2);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(splash.this,MainActivity.class);
+                splash.this.startActivity(mainIntent);
+                splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
